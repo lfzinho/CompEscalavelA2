06dcdd5e35a8e16bc29ad361df6688d748154473
viniciushedler <viniciushedler@gmail.com>
2023-06-21 03:29:33 -0300

    vini spark test

---
+++ SparkTest/python_to_db.py
@@ -0,0 +1,16 @@
+import redis
+
+r = redis.Redis(
+    host='127.0.0.1',
+    port=6379,
+    decode_responses=True
+)
+
+mobile = r.pubsub()
+
+#usando .subscribe() para se inscrever no canal que quer ouvir as mensagens
+mobile.subscribe('veiculo')
+
+# .listen() retorna um gerador sobre o qual você pode iterar e ouvir mensagens do editor
+for message in mobile.listen():
+    print(message) # dá pra fazer o que quiser com message
