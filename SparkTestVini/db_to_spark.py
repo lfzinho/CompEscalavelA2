9e03754b41fc2e196d3b57a153458626c54e468d
viniciushedler <viniciushedler@gmail.com>
2023-06-21 07:07:40 -0300

    ahahhaahaha coringuei

--- SparkTestVini/db_to_spark.py
+++ SparkTestVini/db_to_spark.py
@@ -1,21 +1,23 @@
-import redis
-from pyspark.sql import SparkSession
 import os
 import sys
+import redis
+from pyspark.sql import SparkSession
 from pyspark.sql.functions import split
 
 os.environ['PYSPARK_PYTHON'] = sys.executable
 os.environ['PYSPARK_DRIVER_PYTHON'] = sys.executable
 
-class Reader:
+class Transformer:
 
     def __init__(self) -> None:
+        # simply creates the spark session
         self.spark =  SparkSession.builder \
             .appName("RedisSparkIntegration") \
             .getOrCreate()
         
     
     def read_data_from_redis(self):
+        # connects to redis
         redis_client = redis.Redis(
             host='192.168.0.79',
             port=6381,
@@ -24,41 +26,92 @@ class Reader:
             decode_responses = True,
         )
 
+        # creates a container for tuples of (data, value)
         data = []
 
+        # creates a tuple for every tuple
         for key in redis_client.keys("*"):
             value = redis_client.get(key)
             data.append((key, value))
 
         return data
 
-    def get_all_data(self):
-        redis_client = redis.Redis(
-            host='192.168.0.79',
-            port=6381,
-            password='1234',
-            db=1,
-            decode_responses = True,
-        )
-        all_keys = redis_client.keys('*')
-        for key in all_keys:
-            value = redis_client.get(key)
-            print(f"Key: {key} Value: {value}")
-        #     all_data[key] = value
-        # return all_data
-
     def get_df(self):
+        # gets the data
         data = self.read_data_from_redis()
-        df = self.spark.createDataFrame(data, ["key", "value"])
-        df = df.withColumn('time', split(df['key'], ' ').getItem(0)) \
+
+        # create a spark dataframe with coumns key and value
+        self.df = self.spark.createDataFrame(data, ["key", "value"])
+
+        # now splits the columns key into time and plate and laue into road, lane and length
+        self.df = self.df.withColumn('time', split(df['key'], ' ').getItem(0)) \
             .withColumn('car_plate', split(df['key'], ' ').getItem(1)) \
             .withColumn('road_name', split(df['value'], ' ').getItem(0)) \
             .withColumn('car_lane', split(df['value'], ' ').getItem(1)) \
-            .withColumn('car_length', split(df['value'], ' ').getItem(2)) \
+            .withColumn('car_length', split(df['value'], ' ').getItem(2))
 
-        # df = self.spark.createDataFrame([(data[b"time"], data[b"road_name"], data[b"car_plate"], data[b"car_lane"], data[b"car_length"],)], ["time", "road_name", "car_plate", "car_lane", "car_length",])
-        df.show(n=5)
+    def base_transform(self):
+        pass
+
+    def individual_analysis(self):
+        self.add_analysis1()
+        self.add_analysis2()
+        self.add_analysis3()
+        self.add_analysis4()
+        self.add_analysis5()
+        self.add_analysis6()
+
+    def add_analysis1(self):
+        # n rodovias
+        pass
+
+    def add_analysis2(self):
+        # n veiculos
+        pass
+
+    def add_analysis3(self):
+        # n veiculos risco colisao
+        pass
+
+    def add_analysis4(self):
+        # n veiculos acima velocidade limite
+        pass
+
+    def add_analysis5(self):
+        # lista veiculos acima limite velocidade
+        pass
+
+    def add_analysis6(self):
+        # lista veiculos risco de colisao
+        pass
+
+    def historical_analysis(self):
+        self.add_analysis7()
+        self.add_analysis8()
+        self.add_analysis9()
+
+    def add_analysis7(self):
+        # ranking top 100 veiculos
+        pass
+
+    def add_analysis8(self):
+        # carros proibidos de circular
+        pass
+
+    def add_analysis9(self):
+        # estatistica de cada rodovia
+        pass
+
+    def add_analysis10(self):
+        # lista de carros com direcao perigosa
+        pass
+
+    
+
+    
+
+    
+        
 
 r = Reader()
-# r.get_all_data()
 r.get_df()
